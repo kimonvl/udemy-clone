682e4883c78cec38ba8c6c7a85ef567e540a273c
kimonvl <kimonvloutis@gmail.com>
2025-04-01 00:58:26 +0300

    course progress page ui

--- client/src/pages/student/StudentCourseProgressPage.jsx
+++ client/src/pages/student/StudentCourseProgressPage.jsx
@@ -1,8 +1,13 @@
 import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { Label } from '@/components/ui/label'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import VideoPlayer from '@/components/video-player/VideoPlayer'
 import { selectStudentSelectedCourseProgress } from '@/store/student/student.selector'
 import { fetchCourseProgressStart } from '@/store/student/studentSlice'
+import { Check, ChevronLeft, ChevronRight, Play } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
@@ -23,34 +28,90 @@ const StudentCourseProgressPage = () => {
   }, [courseId])
 
   return (
-    <div className='flex h-screen'>
-      {/* Left Section (1/3) */}
-      <div className="w-1/5 flex flex-col bg-blue-500 flex items-center justify-center text-white text-xl p-4">
-        <h1>{selectedCourse?.title}</h1>
-        <Card className="w-full h-full">
-          <CardContent className="p-4 items-center">
-            {
-              selectedCourse?.lectures.length > 0 && selectedCourse?.lectures.map((lecture) => (
-                <div className='m-2 flex flex-row gap-2 items-center p-2 w-full'>
-                  <Button onClick={() =>setSelectedVideo(lecture.video)} className="w-full">{lecture.title}</Button>
-                  {
-                    lecture.index < selectedCourse.progressIndex ? <span>watched</span> : null
-                  }
-                  
-                </div>
-              ))
-            }
-          </CardContent>
-        </Card>
-
+    <div className='flex flex-col h-screen bg-[#1c1d1f] text-white'>
+      <div className='flex items-center justify-between p-4 bg-[#1c1d1f] border-b border-gray-700'>
+        <div className='flex items-center space-x-4'>
+          <Button className="text-black" variant="ghost" size="sm">
+            <ChevronLeft className='h-4 w-4 mr-2'/>
+            Back to My Courses Page
+          </Button>
+          <h1 className='text-lg font-bold hidden md:block'>
+            Title
+          </h1>
+        </div>
+        <Button>
+          <ChevronRight className='h-5 w-5'/>
+          <ChevronLeft className='h-5 w-5'/>
+        </Button>
       </div>
-
-      <div className="w-4/5 bg-green-500 flex items-center justify-center text-white text-xl p-4">
-        <div className="w-full h-full flex justify-center items-center">
-          {/* Video Player takes most of the space */}
-          <VideoPlayer width="90%" height="80%" url={selectedVideo} />
+      <div className='flex flex-1 overflow-hidden'>
+        <div className={`flex-1 ${true ? "mr-[400px]" : ""} transition-all duration-300`}>
+          <VideoPlayer width='100%' height='500px' url={""}/>
+          <div className='p-6 bg-[#1c1d1f]'>
+            <h2 className='text-2xl font-bold mb-2'>Lecture title</h2>
+          </div>
+        </div>
+        <div className={` w-[400px] bg-[#1c1d1f] border-l border-gray-700 transition-all duration-300 ${true ? "translate-x-0" : "translate-x-full"}`}>
+          <Tabs defaultValue='content' className='h-full flex flex-col'>
+            <TabsList className="grid bg-[#1c1d1f] w-full grid-cols-2 p-0 h-14">
+              <TabsTrigger value="content" className="text-black rounded-none h-full">
+                Course Content
+              </TabsTrigger>
+              <TabsTrigger value="overview" className="text-black rounded-none h-full">
+                Overview
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="content">
+              <ScrollArea className="h-full">
+                <div className='p-4 space-y-4'>
+                  {
+                    [1,2,3,4,5].map((item) => (
+                      <div className='flex items-center space-x-2 text-sm text-white font-bold cursor-pointer'>
+                        {
+                          true ? (<Check className='h-4 w-4 text-green-500'/>) : (
+                            <Play className='h-4 w-4'/>
+                          )
+                        }
+                        <span>lecture title</span>
+                      </div>
+                    ))
+                  }
+                </div>
+              </ScrollArea>
+            </TabsContent>
+            <TabsContent value="overview" className="flex-1 overflow-hidden">
+              <ScrollArea className="h-full">
+                <div className='p-4'>
+                  <h2 className='text-xl font-bold mb-4'>About this course</h2>
+                  <p className='text-gray-400'>Course Description</p>
+                </div>
+              </ScrollArea>
+            </TabsContent>
+          </Tabs>
         </div>
       </div>
+      <Dialog open={false}>
+        <DialogContent className="sm:w-[425px]">
+          <DialogHeader>
+            <DialogTitle>You can't view this page</DialogTitle>
+            <DialogDescription>Please purchase this course to get access</DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={false}>
+        <DialogContent showOverlay={false} className="sm:w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Congratulations!</DialogTitle>
+            <DialogDescription className="flex flex-col gap-3">
+              <Label>You have completed the course</Label>
+              <div className='flex flex-row gap-3'>
+                <Button>My Courses Page</Button>
+                <Button>Rewatch Course</Button>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
